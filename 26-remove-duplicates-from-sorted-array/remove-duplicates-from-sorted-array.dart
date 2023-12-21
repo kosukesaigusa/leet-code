deed550da2043e106fb6fc12d23c480d440c1d31
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-12-21 21:31:19 +0900

    Time: 1649 ms (5.02%) | Memory: 148.8 MB (16.14%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -1,29 +1,28 @@
-// class Solution {
-//   int removeDuplicates(List<int> nums) {
-//     var i = 1;
-//     while (nums.length > i) {
-//       if (nums[i - 1] == nums[i]) {
-//         nums.remove(nums[i]);
-//         continue;
-//       }
-//       i++;
-//     }
-//     return nums.length;
-//   }
-// }
-
-/// 上記よりかなり実行速度が速い！
 class Solution {
   int removeDuplicates(List<int> nums) {
-    var i = 0;
-    while (i < nums.length) {
-      final value = nums[i];
-      while (i + 1 < nums.length && nums[i + 1] == value) {
-        nums.remove(value);
+    var i = 1;
+    while (nums.length > i) {
+      if (nums[i - 1] == nums[i]) {
+        nums.remove(nums[i]);
+        continue;
       }
       i++;
     }
-
     return nums.length;
   }
 }
+
+// class Solution {
+//   int removeDuplicates(List<int> nums) {
+//     var i = 0;
+//     while (i < nums.length) {
+//       final value = nums[i];
+//       while (i + 1 < nums.length && nums[i + 1] == value) {
+//         nums.remove(value);
+//       }
+//       i++;
+//     }
+
+//     return nums.length;
+//   }
+// }
